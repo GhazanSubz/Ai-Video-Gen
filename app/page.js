2f3d406f5b41df1044195ccfc08479e60e768e0f
GhazanSubz <gsubzwari@gmail.com>
2024-10-29 11:02:15 -0700

    auth setup

--- app/page.js
+++ app/page.js
@@ -4,9 +4,9 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h2>Ghazan</h2>
+     
       <Button>Subzwari</Button>
-      <Button>Awais</Button>
+     
     </div>
   );
 }
